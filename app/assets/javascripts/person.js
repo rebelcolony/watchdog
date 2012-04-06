305c7e2512393ed155903bed9f93d58f6f383dfc
rebelcolony <kevin@rebelcolony.com>
2012-04-06 17:09:20 +0200

    bundle update and category labels for people

--- app/assets/javascripts/person.js
+++
@@ -1,8 +0,0 @@
-
-$('input#person_red').change(function(){
-    if($(this).is(":checked")) {
-      $('div.entry_header').addClass("red");
-    } else {
-      $('div.entry_header').addClass("normal");
-    }
-});
