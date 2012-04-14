d9edaea151ed731b41ce4444da34bfb6caf51d49
rebelcolony <kevin@rebelcolony.com>
2012-04-14 16:55:07 +0200

    initial tests update

--- test/integration/only_admin_can_edit_people_test.rb
+++ test/integration/only_admin_can_edit_people_test.rb
@@ -2,12 +2,22 @@ require 'test_helper'
 
 class OnlyAdminCanManagePeopleTest < ActionDispatch::IntegrationTest
   
-  
   test "only_admin_can_edit_person" do
     create_person
     visit logout_path
-    visit edit_person_path
+    visit('/people/1/edit')
     assert page.has_content?("Unauthorized Access")
   end
+  
+  test "only_admin_can_create_person" do
+    visit('/people/new')
+    assert page.has_content?("Unauthorized Access")
+  end
+  
+  test "only_admin_can_create_person" do
+    visit('/people/new')
+    assert page.has_content?("Unauthorized Access")
+  end
+  
   
 end
