ce78b7d5f7a8762a61313f43001857b060bd033f
rebelcolony <kevin@rebelcolony.com>
2012-04-26 21:56:43 +0200

    tidy up after manual merge for search autocomplete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120408075948) do
+ActiveRecord::Schema.define(:version => 20120425153114) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(:version => 20120408075948) do
 
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
-
 
   create_table "people", :force => true do |t|
     t.string   "name"
